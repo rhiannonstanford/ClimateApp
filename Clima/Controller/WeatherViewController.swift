ad6a5f43140fa3dc7e3fdb0cf492b8b9d5c29a53
Rhiannon Stanford <rhiannonstanford@gmail.com>
2022-05-22 12:54:38 -0700

    Fix bug in searchPressed.  Print before endEditing

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -16,35 +16,38 @@ class WeatherViewController: UIViewController,  UITextFieldDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     
     @IBAction func searchPressed(_ sender: UIButton) {
+        print("searchPressed", searchTextField.text!)
         searchTextField.endEditing(true) // dismisses keyboard
-        print("test")
-        print(searchTextField.text!)
-        
+        // user pressec search icon / magnifying glass
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchTextField.delegate = self // the text field should report back to the view controller
+        searchTextField.delegate = self // the text field should report back to the view controller // event listener for the searchTextField
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print(searchTextField.text!)
         return true
+        // user pressed return key
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         // triggered when any text field is done editing
         searchTextField.text = ""
+        // only gets called when they keyboard is hidden, and so then we clear the text field
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
         if textField.text != "" {
             return true
         } else {
             textField.placeholder = "Type something here"
             return false
         }
+        //  should end comes right before the keyboard is dismissed
     }
 
 

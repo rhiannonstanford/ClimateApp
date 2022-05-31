5606fcc149465b18caa4007936459690cf6d80f8
Rhiannon Stanford <rhiannonstanford@gmail.com>
2022-05-31 12:50:41 -0700

    Complete data modeling to consume API data

--- Clima/Model/WeatherData.swift
+++ Clima/Model/WeatherData.swift
@@ -9,12 +9,25 @@
 import Foundation
 
 struct WeatherData: Decodable {
-    let name: String
+    let coord: Coord
     let weather: [Weather]
+    let base: String
     let main: Main
+    let visibility: Double
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Double
+    let sys: Sys
+    let timezone: TimeZone
+    let id: Int
+    let name: String
+    let cod: Int
 }
 
-
+struct Coord: Decodable {
+    let lon: Double
+    let lat: Double
+}
 
 struct Weather: Decodable {
     let id: Double
@@ -23,12 +36,28 @@ struct Weather: Decodable {
     let icon: String
 }
 
-
-
 struct Main: Decodable {
     let temp: Double
-    let pressure: Double
-    let humidity: Double
+    let feels_like: Double
     let temp_min: Double
     let temp_max: Double
+    let pressure: Double
+    let humidity: Double
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Double
+}
+
+struct Clouds: Decodable {
+    let all: Double
+}
+
+struct Sys: Decodable {
+    let type: Int
+    let id: Int
+    let country: String
+    let sunrise: Double
+    let sunset: Double
 }
